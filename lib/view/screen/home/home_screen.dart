00969c84fb39069eef4f75c922a762340f2ac8d8
email <email@example.com>
2024-01-04 15:06:50 +0600

    fist

--- lib/view/screen/home/home_screen.dart
+++ lib/view/screen/home/home_screen.dart
@@ -5,6 +5,10 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.menu,color: Colors.cyan,size: 28,),
+      ),
+    );
   }
 }
